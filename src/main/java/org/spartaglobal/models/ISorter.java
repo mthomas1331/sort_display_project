40cb4812790f9fd177644381b63447151df1353e
mthomas1331 <mthomas6298@outlook.com>
2021-12-17 20:00:57 +0000

    sorter interface complete with comments

--- src/main/java/org/spartaglobal/models/ISorter.java
+++ src/main/java/org/spartaglobal/models/ISorter.java
@@ -1,4 +1,9 @@
 package org.spartaglobal.models;
+
+/**
+ * @implNote This is the sorter interface used for all other sorting classes
+ *           only contains one method which returns an int array
+ */
 public interface ISorter {
     int[] sortArray();
 }
--- src/test/java/org/spartaglobal/ExecutionTests.java
+++
@@ -1,4 +0,0 @@
-package org.spartaglobal;
-
-public class ExecutionTests {
-}
